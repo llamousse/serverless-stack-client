2ebaf96478b8b47846cb717b860840919dec6b8e
Vicky <vyue013@gmail.com>
2020-07-10 17:43:34 -0700

    set up react app, added routes, configured aws amplify

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-upload-13"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://d94cjy8ed2.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_lUbXs9yhw",
+        APP_CLIENT_ID: "nevp33juff9923vjnrt7idktq",
+        IDENTITY_POOL_ID: "us-east-1:f373318a-1e67-48b2-b414-52e76f472733"
+    }
+};
